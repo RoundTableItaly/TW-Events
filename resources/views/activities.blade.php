c778fe13e1638b10eb7cdf02f7ffae1c1774f153
Riccardo Dal Fiume <riccardo@dalfiume.io>
2025-08-20 17:07:26 +0200

    Add Google Analytics

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
   <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-455DX8QQGV"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-455DX8QQGV');
+    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Round Table Italia Events</title>
